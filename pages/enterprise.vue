752c4825d10a2a21ed70caa472e8d11257657746
Roman Gilg <romangg@manjaro.org>
2024-05-26 16:39:13 +0200

    feat: add enterprise page

--- pages/enterprise.vue
+++ pages/enterprise.vue
@@ -1,3 +1,209 @@
 <template>
-  <h1>Enterprice Services page</h1>
+  <div>
+    <div class="relative flex flex-col-reverse px-4 py-16 mx-auto lg:block lg:flex-col lg:py-32 xl:py-48 md:px-8 sm:max-w-xl md:max-w-full">
+      <div class="z-0 flex justify-center h-full -mx-4 overflow-hidden lg:pt-24 lg:pb-16 lg:pr-8 xl:pr-0 lg:w-1/2 lg:absolute lg:justify-end lg:bottom-0 lg:left-0 lg:items-center">
+        <img
+          src="public/services.png"
+          class="lg:pr-10 object-right w-full h-auto lg:w-auto lg:h-full"
+          alt=""
+        >
+      </div>
+      <div class="relative flex justify-end max-w-xl mx-auto xl:pr-32 lg:max-w-screen-xl">
+        <div class="mb-16 lg:pr-5 lg:max-w-lg lg:mb-0">
+          <div class="max-w-xl mb-6">
+            <div>
+              <p class="inline-block py-px mb-4 text-xl font-bold tracking-wider text-gray-800 uppercase rounded-full">
+                Enterprise Services
+              </p>
+            </div>
+            <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+              Based on open source
+            </h2>
+            <p class="text-base text-gray-700 md:text-lg">
+              Providing SME's and public institutions with holistic, secure open source solutions
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-6 lg:py-8">
+      <div class="grid gap-4 row-gap-5 sm:grid-cols-2 lg:grid-cols-4">
+        <div class="flex flex-col justify-between p-5 border rounded shadow-sm bg-amber-200 relative">
+          <div>
+            <h6 class="mb-2 font-semibold leading-5">
+              Hardware
+            </h6>
+            <p class="mb-3 text-sm text-gray-900">
+              Manjaro workstations, thin clients or laptops from our partners to suit your needs.
+            </p>
+          </div>
+        </div>
+        <div class="flex flex-col justify-between p-5 border rounded shadow-sm bg-amber-200">
+          <div>
+            <h6 class="mb-2 font-semibold leading-5">
+              Servers
+            </h6>
+            <p class="mb-3 text-sm text-gray-900">
+              Open source cloud and on-premise server infrastructures.
+            </p>
+          </div>
+        </div>
+        <div class="flex flex-col justify-between p-5 border rounded shadow-sm bg-amber-200">
+          <div>
+            <h6 class="mb-2 font-semibold leading-5">
+              Conversions
+            </h6>
+            <p class="mb-3 text-sm text-gray-900">
+              Convert workstations based on proprietary software to Manjaro, and alternatives for running internal services.
+            </p>
+          </div>
+        </div>
+        <div class="flex flex-col justify-between p-5 border rounded shadow-sm bg-amber-200">
+          <div>
+            <h6 class="mb-2 font-semibold leading-5">
+              AfterMarket
+            </h6>
+            <p class="mb-3 text-sm text-gray-900">
+              Maintained installations and integration with internal platforms and server infrastructure.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+        <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          <span class="relative inline-block" />
+          Why choose us
+        </h2>
+      </div>
+      <div class="grid gap-5 mb-8 md:grid-cols-2 lg:grid-cols-3">
+        <div class="p-5 duration-300 transform bg-white border rounded shadow-sm hover:-translate-y-2">
+          <div class="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+            <svg
+              class="w-10 h-10 text-deep-purple-accent-400"
+              stroke="currentColor"
+              viewBox="0 0 52 52"
+            >
+              <polygon
+                stroke-width="3"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                fill="none"
+                points="29 13 14 29 25 29 23 39 38 23 27 23"
+              />
+            </svg>
+          </div>
+          <h6 class="mb-2 font-semibold leading-5">
+            Cascading stability
+          </h6>
+          <p class="text-sm text-gray-900">
+            Our unique operating system approach combines the agility of an rolling release with the robustness's and dependability of an enterprise grade one. On top of a Linux kernel
+          </p>
+        </div>
+        <div class="p-5 duration-300 transform bg-white border rounded shadow-sm hover:-translate-y-2">
+          <div class="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+            <svg
+              class="w-10 h-10 text-deep-purple-accent-400"
+              stroke="currentColor"
+              viewBox="0 0 52 52"
+            >
+              <polygon
+                stroke-width="3"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                fill="none"
+                points="29 13 14 29 25 29 23 39 38 23 27 23"
+              />
+            </svg>
+          </div>
+          <h6 class="mb-2 font-semibold leading-5">
+            Friendly and qualified technicians
+          </h6>
+          <p class="text-sm text-gray-900">
+            Our customers always have access to knowledgeable service personal.
+          </p>
+        </div>
+        <div class="p-5 duration-300 transform bg-white border rounded shadow-sm hover:-translate-y-2">
+          <div class="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+            <svg
+              class="w-10 h-10 text-deep-purple-accent-400"
+              stroke="currentColor"
+              viewBox="0 0 52 52"
+            >
+              <polygon
+                stroke-width="3"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                fill="none"
+                points="29 13 14 29 25 29 23 39 38 23 27 23"
+              />
+            </svg>
+          </div>
+          <h6 class="mb-2 font-semibold leading-5">
+            European data protection
+          </h6>
+          <p class="text-sm text-gray-900">
+            The manjaro company is based in Germany, our servers are too. European Data Protection laws guarantee that your data is safe with us.
+          </p>
+        </div>
+        <div class="p-5 duration-300 transform bg-white border rounded shadow-sm hover:-translate-y-2">
+          <div class="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+            <svg
+              class="w-10 h-10 text-deep-purple-accent-400"
+              stroke="currentColor"
+              viewBox="0 0 52 52"
+            >
+              <polygon
+                stroke-width="3"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                fill="none"
+                points="29 13 14 29 25 29 23 39 38 23 27 23"
+              />
+            </svg>
+          </div>
+          <h6 class="mb-2 font-semibold leading-5">
+            Software you can trust
+          </h6>
+          <p class="text-sm text-gray-900">
+            Manjaro workstation and applications together with debian server operating systems hight level of trust.
+          </p>
+        </div>
+        <div class="p-5 duration-300 transform bg-white border rounded shadow-sm hover:-translate-y-2">
+          <div class="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+            <svg
+              class="w-10 h-10 text-deep-purple-accent-400"
+              stroke="currentColor"
+              viewBox="0 0 52 52"
+            >
+              <polygon
+                stroke-width="3"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                fill="none"
+                points="29 13 14 29 25 29 23 39 38 23 27 23"
+              />
+            </svg>
+          </div>
+          <h6 class="mb-2 font-semibold leading-5">
+            hardware you can rely on
+          </h6>
+          <p class="text-sm text-gray-900">
+            We work together with reputable Linux hardware vendors like TUXEDO, Orange Pi and Slimbook.
+          </p>
+        </div>
+      </div>
+      <div class="text-center">
+        <a
+          href="/"
+          class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+        >
+          Learn more
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
