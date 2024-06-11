36899def60912c62e92a308e01552313e8289fc9
Roman Gilg <subdiff@gmail.com>
2024-06-11 22:04:49 +0300

    fix: correct colors in dark mode

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="bg-white dark:bg-gray-900">
+  <section>
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
         <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
