cf1f34575522e7cfd267ca5a7cfb4d900c310e8f
Roman Gilg <subdiff@gmail.com>
2024-06-13 20:19:55 +0300

    build: add NuxtImage module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxtjs/tailwindcss',
-  ],
+  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxt/image'],
   eslint: {
     checker: true,
     config: {
