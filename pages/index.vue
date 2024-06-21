3a2d5330a509a5d4e86b64b6c4d834ae2c8901cc
Roman Gilg <romangg@manjaro.org>
2024-06-22 00:40:04 +0200

    fix: correct main page layout flow and spacing

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-    <div class="lg:min-h-[560px] bg-primary px-4 sm:px-10">
-      <div class="max-w-7xl w-full mx-auto py-16 text-white">
-        <div class="grid lg:grid-cols-2 justify-center items-center gap-10">
+    <div class="lg:min-h-[520px] bg-primary px-4 flex">
+      <div class="px-4 sm:px-10 max-w-7xl mx-auto py-12 text-white place-self-center">
+        <div class="grid lg:grid-cols-2 justify-center items-center gap-14">
+          <div class="max-lg:mt-12 h-full place-self-center lg:order-last">
+            <NuxtImg
+              src="desktops.png"
+              alt="Gnome, KDE desktops"
+              width="700px"
+            />
+          </div>
           <div>
             <h1 class="md:text-5xl text-4xl font-bold mb-6 md:!leading-[55px]">
               Manjaro Linux Empowering People and
@@ -23,19 +30,20 @@
               </NuxtLink>
             </div>
           </div>
-          <div class="max-lg:mt-12 h-full place-self-center">
-            <NuxtImg
-              src="desktops.png"
-              alt="Gnome, KDE desktops"
-              width="700px"
-            />
-          </div>
         </div>
       </div>
     </div>
 
-    <div class="px-4 sm:px-10 mt-28">
+    <div class="px-4 sm:px-10 mt-14 md:mt-20">
       <div class="max-w-7xl w-full mx-auto grid md:grid-cols-2 gap-10 place-items-center">
+        <div class="h-full md:order-last">
+          <NuxtImg
+            src="branch.png"
+            alt="feature"
+            width="300px"
+            class="h-full"
+          />
+        </div>
         <div>
           <h2 class="md:text-4xl text-3xl font-semibold mb-6">
             Robust with Rolling Releases
@@ -46,44 +54,34 @@
             Manjaro that fits your needs.
           </p>
         </div>
-        <div class="h-full">
-          <NuxtImg
-            src="branch.png"
-            alt="feature"
-            height="300px"
-            class="h-full"
-          />
-        </div>
       </div>
     </div>
 
-    <div class="px-4 sm:px-10 mt-28 py-10">
-      <div class="max-w-7xl w-full mx-auto">
-        <div class="grid md:grid-cols-2 items-center gap-10">
-          <div class="w-full h-full">
-            <NuxtImg
-              src="company.jpg"
-              height="500px"
-              class="w-full h-full object-cover"
-            />
+    <div class="px-4 sm:px-10 mt-14 md:mt-28">
+      <div class="max-w-7xl w-full mx-auto grid md:grid-cols-2 gap-10 place-items-center">
+        <div class="w-full h-full">
+          <NuxtImg
+            src="company.jpg"
+            height="500px"
+            class="w-full h-full object-cover"
+          />
+        </div>
+        <div>
+          <div class="text-background">
+            <h2 class="md:text-4xl text-3xl font-semibold mb-6">
+              Enterprise Grade
+            </h2>
+            <p>
+              Manjaro comes in different versions suitable for home but also business use. The Manjaro company provides first-class services to provide businesses with the means to make efficient use of their IT infrastructure leveraging Manjaro Linux.
+            </p>
           </div>
-          <div>
-            <div class="text-background">
-              <h2 class="md:text-4xl text-3xl font-semibold mb-6">
-                Enterprise Grade
-              </h2>
-              <p>
-                Manjaro comes in different versions suitable for home but also business use. The Manjaro company provides first-class services to provide businesses with the means to make efficient use of their IT infrastructure leveraging Manjaro Linux.
-              </p>
-            </div>
-            <NuxtLink href="/enterprise">
-              <button
-                class="flex items-center px-5 py-4 mt-8 btn"
-              >
-                Learn More
-              </button>
-            </NuxtLink>
-          </div>
+          <NuxtLink href="/enterprise">
+            <button
+              class="flex items-center px-5 py-4 mt-8 btn"
+            >
+              Learn More
+            </button>
+          </NuxtLink>
         </div>
       </div>
     </div>
