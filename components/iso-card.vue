79ea3d2c63d0615ec42c93657f8e93fb59d1a6de
Roman Gilg <romangg@manjaro.org>
2024-06-21 10:08:37 +0200

    feat: add download pages

---
+++ components/iso-card.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="card max-w-80 bg-base-100 shadow-xl">
+    <div v-if="!showDetails">
+      <figure>
+        <NuxtImg
+          :src="'/desktops/' + desktopId + '.webp'"
+          alt="Desktop image"
+          class="h-40"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">
+          {{ desktopData.name }}
+        </h2>
+        <p>{{ desktopData.description }}</p>
+        <div class="flex flex-row space-x-5 mt-4">
+          <div class="card-actions justify-end">
+            <NuxtLink :href="getDownloadLink()">
+              <button class="btn btn-primary">
+                Download
+              </button>
+            </NuxtLink>
+          </div>
+          <div
+            v-if="!isoData.custom"
+            class="card-actions justify-end"
+          >
+            <button
+              class="btn"
+              @click="toggleDetails"
+            >
+              More
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="showDetails">
+      <div class="card-body">
+        <h2 class="card-title">
+          {{ desktopData.name }}
+        </h2>
+        <div class="flex flex-row space-x-2 justify-center pt-4">
+          <div class="font-bold">
+            Minimal
+          </div>
+          <input
+            type="checkbox"
+            class="toggle toggle-primary"
+            :checked="fullImage"
+            @click="toggleFullImage"
+          >
+          <div class="font-bold">
+            Full
+          </div>
+        </div>
+        <div class="flex grid grid-cols-2 gap-4 pt-8">
+          <button class="btn btn-secondary">
+            <NuxtLink :href="getDetailEntry('torrent')">
+              Torrent
+            </NuxtLink>
+          </button>
+          <button class="btn btn-primary">
+            <NuxtLink :href="getDetailEntry('image')">
+              Image
+            </NuxtLink>
+          </button>
+          <button class="btn btn-accent">
+            <NuxtLink :href="getDetailEntry('signature')">
+              Signature
+            </NuxtLink>
+          </button>
+          <button class="btn btn-info">
+            <NuxtLink :href="getDetailEntry('checksum')">
+              Checksum
+            </NuxtLink>
+          </button>
+        </div>
+        <div class="flex flex-row space-x-5 mt-8">
+          <div class="card-actions justify-end">
+            <button
+              class="btn"
+              @click="toggleDetails"
+            >
+              Back
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  desktopId: { type: String, required: true },
+  desktopData: { type: Object, required: true },
+  isoData: { type: Object, required: true } },
+)
+
+function getDownloadLink() {
+  if (props.isoData.custom) {
+    return props.isoData.custom
+  }
+  if (props.isoData.minimal) {
+    return props.isoData.minimal.image
+  }
+  return props.isoData.image
+}
+
+const showDetails = ref(false)
+const fullImage = ref(false)
+
+function toggleDetails() {
+  showDetails.value = !showDetails.value
+}
+function toggleFullImage() {
+  fullImage.value = !fullImage.value
+}
+
+const getDetailEntry = (entry: string) => {
+  if (fullImage.value) {
+    return props.isoData[entry]
+  }
+  if (props.isoData.minimal) {
+    return props.isoData.minimal[entry]
+  }
+  return props.isoData[entry]
+}
+</script>
--- pages/download-arm.vue
+++ pages/download-arm.vue
@@ -1,3 +1,68 @@
 <template>
-  <h1>Arm Download Page</h1>
+  <section class="text-gray-600 body-font dark:text-gray-400">
+    <div class="container px-5 pt-24 mx-auto">
+      <div class="flex flex-col text-center w-full mb-14">
+        <h1 class="text-2xl font-medium title-font mb-4 text-gray-900 dark:text-gray-200 tracking-widest">
+          ARM Images
+        </h1>
+        <p class="lg:w-2/3 mx-auto leading-relaxed">
+          Provided by the Manjaro ARM Team
+        </p>
+      </div>
+    </div>
+    <div class="container mx-auto pb-10 flex justify-center items-center">
+      <select
+        v-model="selectedDevice"
+        class="select select-bordered w-full max-w-xs"
+      >
+        <option
+          v-for="(device, deviceName) of isos.arm"
+          :key="deviceName"
+          :value="deviceName"
+        >
+          {{ deviceName }}
+        </option>
+      </select>
+    </div>
+    <div class="container mx-auto pb-10 px-5 flex justify-center items-center">
+      <div class="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-20">
+        <IsoCard
+          v-for="(iso, title) of isos.arm[selectedDevice]"
+          :key="title"
+          :desktop-id="title"
+          :desktop-data="getDesktopData(title)"
+          :iso-data="iso"
+        />
+      </div>
+    </div>
+
+    <div class="container px-5 pt-24 mx-auto">
+      <div class="flex flex-col text-center w-full mb-14">
+        <NuxtLink href="/download">
+          <p class="lg:w-2/3 mx-auto leading-relaxed">
+            Click here for X86 images
+          </p>
+        </NuxtLink>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script setup lang="ts">
+import desktops from '~/assets/desktops.json'
+
+useHead({
+  title: 'Download ARM' + ' – manjaro.org',
+})
+
+const { data } = await useFetch('https://gitlab.manjaro.org/api/v4/projects/12597/repository/files/file-info.json/raw', {
+  query: { ref: 'master' },
+})
+const isos = JSON.parse(data?.value as string)
+
+const getDesktopData = (title: string) => {
+  return desktops[title]
+}
+
+const selectedDevice = ref('Generic')
+</script>
