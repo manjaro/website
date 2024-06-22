3a2c3b7914dbf24693794fcff005a964c1f87fe5
Roman Gilg <romangg@manjaro.org>
2024-06-22 03:37:53 +0200

    build: add commitlint dev dependency
    Run with: pnpm commitlint --from <sha>

---
+++ commitlint.config.ts
@@ -0,0 +1,43 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'body-max-line-length': [2, 'always', 90],
+    'footer-leading-blank': [2, 'always'],
+    'footer-max-line-length': [2, 'always', 90],
+    'header-max-length': [2, 'always', 90],
+    'scope-case': [2, 'always', 'lower-case'],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'content',
+      ],
+    ],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
+}
