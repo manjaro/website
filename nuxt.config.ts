394ed500f649fc445dbb4ff5ec43188c2ddd2d80
Roman Gilg <romangg@manjaro.org>
2024-06-22 14:36:35 +0200

    feat: add dark mode toggle

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxt/image'],
+  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxt/image', '@nuxtjs/color-mode'],
   eslint: {
     checker: true,
     config: {
       stylistic: true,
     },
+  },
+  colorMode: {
+    preference: 'system',
+    dataValue: 'theme', // for daisyUI
   },
 })
