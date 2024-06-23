f992e88e41eacd18d80045c2539594f6e25abeff
Roman Gilg <romangg@manjaro.org>
2024-06-23 14:12:49 +0200

    feat: define seo metadata

--- app.vue
+++ app.vue
@@ -5,3 +5,14 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Manjaro – The Linux for People and Organizations',
+  ogTitle: 'Manjaro Linux Official',
+  description: 'This page provides an overview of Manjaro Linux, an open source operating system designed for ease of use. Learn about its features, installation, and support options. Get the most out of Manjaro Linux with the latest news, downloads, and tips from our helpful community.',
+  ogImage: 'https://manjaro.org/meta-image.jpg',
+  twitterCard: 'summary',
+  twitterImage: 'https://manjaro.org/meta-image.jpg',
+})
+</script>
