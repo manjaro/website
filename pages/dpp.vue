868570ca0950b92da5fd429cca52e8b07703e93e
Roman Gilg <romangg@manjaro.org>
2024-06-25 23:03:21 +0200

    feat: add DPP info

--- pages/dpp.vue
+++ pages/dpp.vue
@@ -1,19 +1,312 @@
 <template>
-  <section class="text-gray-600 body-font dark:text-gray-400">
-    <div class="container px-5 pt-24 mx-auto">
-      <div class="flex flex-col text-center w-full mb-20">
-        <h1 class="text-2xl font-medium title-font mb-4 text-gray-900 dark:text-gray-200 tracking-widest">
+  <section class="max-w-screen-xl mx-auto">
+    <div class="container pt-12 mx-auto px-6 xl:px-12">
+      <div class="flex flex-col text-center mb-14">
+        <h1 class="text-3xl font-medium mb-4">
           Device Partner Program
         </h1>
-        <p class="lg:w-2/3 mx-auto leading-relaxed">
+        <p class="lg:w-2/3 mx-auto leading-relaxed text-gray-700 dark:text-gray-400">
           Design and market your hardware device together with Manjaro.
         </p>
-        <NuxtImg
-          src="/construction.png"
-          alt=""
-          width="200"
-          class="object-contain h-52 pt-8 grayscale place-self-center"
-        />
+      </div>
+      <div class="mx-auto pb-8 lg:pb-14 max-w-[400px] sm:max-w-max sm:px-0">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 items-start">
+          <div class="p-4 border-2 border-accent dark:border-accent-200 rounded-lg">
+            <div class="flex flex-col items-center pt-5">
+              <div class="flex -space-x-3 pb-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="60"
+                  height="60"
+                  fill="currentColor"
+                  class="text-accent dark:text-accent-600"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767z"
+                  />
+                </svg>
+              </div>
+              <h2 class="text-xl tracking-wide font-bold text-primary-300 dark:text-primary-800 uppercase">
+                Basic
+              </h2>
+            </div>
+            <div class="divider" />
+            <div class="text-center">
+              <h4 class="uppercase font-semibold text-primary pb-2">
+                Engineering
+              </h4>
+              <ul class="text-gray-800 dark:text-gray-300">
+                <li>Support per device family</li>
+                <li>Regular image releases</li>
+                <li>QA/Testing support</li>
+              </ul>
+              <h4 class="uppercase font-semibold text-primary text-center pt-8 pb-2">
+                Marketing
+              </h4>
+              <ul class="text-gray-800 dark:text-gray-300">
+                <li>Regular social media posts about progress</li>
+              </ul>
+            </div>
+          </div>
+          <div class="p-4 border-2 border-accent dark:border-accent-200 rounded-lg sm:row-start-2 sm:col-start-1 lg:row-auto lg:col-auto">
+            <div class="flex flex-col items-center pt-5">
+              <div class="flex -space-x-3 pb-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="60"
+                  height="60"
+                  fill="currentColor"
+                  class="text-accent dark:text-accent-600"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767z"
+                  />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="60"
+                  height="60"
+                  fill="currentColor"
+                  class="text-accent dark:text-accent-600"
+                  viewBox="0 0 16 16"
+                  transform="rotate(180, 0, 0)"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767z"
+                  />
+                </svg>
+              </div>
+              <h2 class="text-xl tracking-wide font-bold text-primary-300 dark:text-primary-800 uppercase">
+                Mid
+              </h2>
+            </div>
+            <div class="divider" />
+            <div class="text-center">
+              <h4 class="uppercase font-semibold text-primary pb-2">
+                Engineering
+              </h4>
+              <ul class="text-gray-800 dark:text-gray-300">
+                <li>Support per device family</li>
+                <li>Regular image releases</li>
+                <li>QA/Testing support</li>
+              </ul>
+              <div class="flex justify-center p-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  class="text-accent dark:text-accent-600"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+                </svg>
+              </div>
+              <ul class="text-gray-800 dark:text-gray-300">
+                <li>OS device optimization</li>
+                <li>Regular device testing</li>
+                <li>Prototype consulting</li>
+              </ul>
+              <h4 class="uppercase font-semibold text-primary text-center pt-8 pb-2">
+                Marketing
+              </h4>
+              <ul class="text-gray-800 dark:text-gray-300">
+                <li>Regular social media posts about progress</li>
+              </ul>
+              <div class="flex justify-center p-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  class="text-accent dark:text-accent-600"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+                </svg>
+              </div>
+              <ul class="text-gray-800 dark:text-gray-300">
+                <li>Presentation at fairs and conferences</li>
+              </ul>
+            </div>
+          </div>
+          <div class="p-4 border-2 border-accent dark:border-accent-200 rounded-lg sm:row-span-2 sm:col-start-2 sm:row-start-1 lg:row-auto lg:col-auto">
+            <div class="flex flex-col items-center pt-5">
+              <div class="flex -space-x-3 pb-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="60"
+                  height="60"
+                  fill="currentColor"
+                  class="text-accent dark:text-accent-600"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767z"
+                  />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="60"
+                  height="60"
+                  fill="currentColor"
+                  class="text-accent dark:text-accent-600"
+                  viewBox="0 0 16 16"
+                  transform="rotate(180, 0, 0)"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767z"
+                  />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="60"
+                  height="60"
+                  fill="currentColor"
+                  class="text-accent dark:text-accent-600"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767z"
+                  />
+                </svg>
+              </div>
+              <h2 class="text-xl tracking-wide font-bold text-primary-300 dark:text-primary-800 uppercase">
+                Large
+              </h2>
+            </div>
+            <div class="divider" />
+            <div class="text-center">
+              <h4 class="uppercase font-semibold text-primary pb-2">
+                Engineering
+              </h4>
+              <ul class="text-gray-800 dark:text-gray-300">
+                <li>Support per device family</li>
+                <li>Regular image releases</li>
+                <li>QA/Testing support</li>
+              </ul>
+              <div class="flex justify-center p-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  class="text-accent dark:text-accent-600"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+                </svg>
+              </div>
+              <ul class="text-gray-800 dark:text-gray-300">
+                <li>OS device optimization</li>
+                <li>Regular device testing</li>
+                <li>Prototype consulting</li>
+              </ul>
+              <div class="flex justify-center p-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  class="text-accent dark:text-accent-600"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+                </svg>
+              </div>
+              <ul class="text-gray-800 dark:text-gray-300">
+                <li>Engineering sprints at manufacturer</li>
+              </ul>
+              <h4 class="uppercase font-semibold text-primary text-center pt-8 pb-2">
+                Marketing
+              </h4>
+              <ul class="text-gray-800 dark:text-gray-300">
+                <li>Regular social media posts about progress</li>
+              </ul>
+              <div class="flex justify-center p-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  class="text-accent dark:text-accent-600"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+                </svg>
+              </div>
+              <ul class="text-gray-800 dark:text-gray-300">
+                <li>Presentation at fairs and conferences</li>
+              </ul>
+              <div class="flex justify-center p-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  class="text-accent dark:text-accent-600"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+                </svg>
+              </div>
+              <ul class="text-gray-800 dark:text-gray-300">
+                <li>YouTube marketing</li>
+                <li>Partner promotion in EU/US</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div
+          class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:pt-24"
+        >
+          <div class="max-w-xl mb-6 md:mx-auto sm:text-center lg:max-w-2xl">
+            <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-primary uppercase rounded-full bg-teal-accent-400">
+              Contact Us
+            </p>
+            <h2 class="text-2xl font-medium">
+              Ready Your Device With Manjaro
+            </h2>
+          </div>
+          <div class="flex justify-center">
+            <div class="rounded shadow-sm dark:shadow-slate-800 md:text-center max-w-sm">
+              <div class="relative">
+                <NuxtImg
+                  class="object-contain h-64 rounded-t"
+                  width="500px"
+                  src="/dpp-contact.webp"
+                  alt=""
+                />
+                <div class="absolute inset-0 bg-gray-800 bg-opacity-10 dark:bg-opacity-40" />
+              </div>
+              <div class="px-6 py-8 border border-t-0 dark:border-gray-700 rounded-b sm:px-8">
+                <p class="mb-5">
+                  Reach out to learn more about how your device can become part of Manjaro’s Device Partner Program including pricing and availability.
+                </p>
+                <div class="flex justify-center gap-4">
+                  <button
+                    class="btn btn-primary"
+                  >
+                    <NuxtLink
+                      href="mailto:dpp@manjaro.org"
+                      target="_blank"
+                    >
+                      Email
+                    </NuxtLink>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
