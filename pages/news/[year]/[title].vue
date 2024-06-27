000ce47c418ec8e379df6adbaac333890d944ad7
Roman Gilg <romangg@manjaro.org>
2024-06-27 13:57:32 +0200

    feat: show additional data in news articles

--- pages/news/[year]/[title].vue
+++ pages/news/[year]/[title].vue
@@ -1,24 +1,38 @@
 <template>
-  <div class="max-w-screen-sm mx-auto px-6">
-    <article>
-      <div class="py-10">
-        <NuxtImg
-          :src="imgPath"
-          alt="Header image"
-          width="600px"
-        />
-      </div>
-      <h1 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-400">
-        {{ data!.title }}
-      </h1>
-      <section class="prose dark:prose-invert">
-        <ContentRenderer :value="data" />
-      </section>
-    </article>
-  </div>
+  <article class="max-w-screen-sm mx-auto px-6 pt-12">
+    <div class="pb-8">
+      <NuxtImg
+        :src="imgPath"
+        alt="Header image"
+        width="600px"
+      />
+    </div>
+    <h1 class="mb-6 text-3xl font-semibold tracking-tight">
+      {{ data!.title }}
+    </h1>
+    <p class="mb-5 text-gray-800 dark:text-gray-300">
+      {{ data!.description }}
+    </p>
+    <div class="pb-2 flex items-center text-gray-500">
+      {{ useDateFormat(data!.date, 'MMMM DD, YYYY').value }}, by
+      <NuxtImg
+        v-if="authorPicture"
+        class="mx-2 w-9 h-9 rounded-full object-cover"
+        :src="authorPicture"
+        width="50px"
+        alt="Author portrait"
+      />
+      {{ author.name }}
+    </div>
+    <section class="text-gray-900 dark:text-gray-200 prose dark:prose-invert">
+      <ContentRenderer :value="data" />
+    </section>
+  </article>
 </template>
 
 <script setup lang="ts">
+import { useDateFormat } from '@vueuse/core'
+
 const route = useRoute()
 const { data } = await useAsyncData(`news-item-${route.path}`, () => queryContent(route.path).findOne())
 
@@ -27,6 +41,8 @@ if (!data.value) {
 }
 
 const imgPath = route.path + '/' + data.value.image
+const author = getAuthors(data.value.authors)[0]
+const authorPicture = getAuthorPicture(data.value.authors[0])
 
 useHead({
   title: data.value.title + ' | Manjaro News',
