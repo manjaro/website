9c990954e39b1f239ec6b0341083d9172b7f1b42
Roman Gilg <romangg@manjaro.org>
2024-06-27 21:40:03 +0200

    fix: improve news list layout

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,83 +1,41 @@
 <template>
-  <section>
-    <div class="py-12 px-4 mx-auto max-w-screen-xl body-font">
-      <div class="flex flex-col text-center w-full mb-14">
-        <h1 class="text-3xl font-medium mb-4">
-          News and Announcements
-        </h1>
-        <p class="lg:w-2/3 mx-auto leading-relaxed text-gray-700 dark:text-gray-400">
-          Articles about development and organization of the Manjaro project
-        </p>
-      </div>
-      <div class="grid grid-cols-2 gap-10 px-8">
-        <div
-          v-for="item of data"
-          :key="item.id"
-          class="flex flex-col p-6 rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
-        >
-          <div class="flex justify-between items-center mb-5 text-gray-500">
-            <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800 capitalize">
-              <svg
-                class="mr-1 w-3 h-3"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              ><path
-                fill-rule="evenodd"
-                d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-                clip-rule="evenodd"
-              /><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" /></svg>
-              {{ item.kind }}
-            </span>
-            <span class="text-sm">{{ useTimeAgo(item.date).value }}</span>
-          </div>
-          <div class="pb-5 h-[340px]">
-            <NuxtLink :href="item._path">
-              <NuxtImg
-                :src="getImagePath(item._path!, item.image!)"
-                width="600"
-              />
-            </NuxtLink>
-          </div>
-          <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            <NuxtLink :href="item._path">
-              {{ item.title }}
-            </NuxtLink>
-          </h2>
-          <p class="mb-5 text-gray-700 dark:text-gray-300 flex-grow">
+  <section class="py-12 px-4 mx-auto max-w-screen-xl body-font">
+    <div class="flex flex-col text-center w-full mb-14">
+      <h1 class="text-3xl font-medium mb-4">
+        News and Announcements
+      </h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-gray-700 dark:text-gray-400">
+        Articles about development and organization of the Manjaro project
+      </p>
+    </div>
+
+    <div class="mx-auto max-w-[400px] md:max-w-[820px] xl:max-w-max font-[sans-serif] grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+      <NuxtLink
+        v-for="item of data"
+        :key="item.id"
+        :href="item._path!"
+        class="flex flex-col rounded overflow-hidden border dark:border-gray-700 transform hover:scale-[1.02] transition-transform duration-300"
+      >
+        <div>
+          <NuxtImg
+            :src="getImagePath(item._path!, item.image!)"
+            alt="News article header picture"
+            class="w-full h-52 object-cover"
+            width="500px"
+          />
+        </div>
+        <div class="flex flex-col justify-start p-4 lg:p-5 h-full">
+          <span class="text-sm block text-gray-400 dark:text-gray-500 mb-2 capitalize">
+            {{ useTimeAgo(item.date).value }} | {{ getAuthors(item.authors)[0].name }}
+          </span>
+          <h3 class="text-lg font-bold pb-3">
+            {{ item.title }}
+          </h3>
+          <p class="text-gray-600 dark:text-gray-300 text-sm line-clamp-2 ">
             {{ item.description }}
           </p>
-          <div class="flex justify-between">
-            <div class="flex items-center space-x-4">
-              <img
-                v-if="getAuthorPicture(item.authors[0])"
-                class="w-7 h-7 rounded-full"
-                :src="getAuthorPicture(item.authors[0])"
-                alt="Author portrait"
-              >
-              <span class="font-medium">
-                {{ getAuthors(item.authors)[0].name }}
-              </span>
-            </div>
-            <NuxtLink
-              :href="item._path"
-              class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline"
-            >
-              Read more
-              <svg
-                class="ml-2 w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              ><path
-                fill-rule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              /></svg>
-            </NuxtLink>
-          </div>
         </div>
-      </div>
+      </NuxtLink>
     </div>
   </section>
 </template>
