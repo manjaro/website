8948d12e772cc018d5a556b20fc683e88892fbae
Roman Gilg <romangg@manjaro.org>
2024-06-28 12:16:20 +0200

    feat: show abstract person icon as fallback

--- components/team-card.vue
+++ components/team-card.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center sm:text-left">
-    <NuxtImg
-      alt="team"
-      width="300px"
-      class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
-      :src="getPicture(member.id, member.image)"
-    />
+    <div class="flex-shrink-0 w-48 h-48 sm:mb-0 mb-4">
+      <NuxtImg
+        v-if="member.image"
+        alt="team"
+        width="300px"
+        class="rounded-lg w-full h-full object-cover object-center"
+        :src="'/team/' + member.id + '.' + member.image"
+      />
+      <div
+        v-if="!member.image"
+        class="flex justify-center w-full h-full text-gray-400 bg-gray-200 dark:text-gray-600 dark:bg-gray-900 rounded-lg"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+          class="w-[80%]"
+        >
+          <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+        </svg>
+      </div>
+    </div>
     <div class="flex-grow sm:pl-8 min-h-32">
       <h2 class="title-font font-medium text-lg text-gray-900 dark:text-gray-200">
         {{ member.name }}
@@ -29,13 +45,4 @@
 defineProps({
   member: { type: Object, required: true },
 })
-
-const getPicture = (user_id: string, file_type: string) => {
-  if (file_type) {
-    return '/team/' + user_id + '.' + file_type
-  }
-  else {
-    return '/team/jaro-avatar.png'
-  }
-}
 </script>
