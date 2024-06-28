4f61681dea465626f65bf2d00ce2a9e97b169bb9
Roman Gilg <romangg@manjaro.org>
2024-06-28 17:42:49 +0200

    feat: add breadcrumb navigation to news articles

--- pages/news/[year]/[title].vue
+++ pages/news/[year]/[title].vue
@@ -1,81 +1,107 @@
 <template>
-  <article class="max-w-screen-sm mx-auto px-6 pt-12">
-    <div class="pb-8">
-      <NuxtImg
-        :src="imgPath"
-        alt="Header image"
-        width="600px"
-      />
+  <div class="max-w-screen-xl mx-auto">
+    <div class="mx-auto pt-6 px-6 breadcrumbs text-gray-600 dark:text-gray-500">
+      <ul>
+        <li>
+          <NuxtLink
+            class="flex gap-1.5"
+            href="/news"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="text-gray-500"
+              viewBox="0 0 16 16"
+            >
+              <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5z" />
+              <path d="M2 3h10v2H2zm0 3h4v3H2zm0 4h4v1H2zm0 2h4v1H2zm5-6h2v1H7zm3 0h2v1h-2zM7 8h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2z" />
+            </svg>
+            News
+          </NuxtLink>
+        </li>
+        <li>{{ data.title }}</li>
+      </ul>
     </div>
-    <h1 class="mb-6 text-3xl font-semibold tracking-tight">
-      {{ data!.title }}
-    </h1>
-    <p class="mb-5 text-gray-800 dark:text-gray-300">
-      {{ data!.description }}
-    </p>
-    <div class="pb-2 flex flex-row items-center text-gray-500">
-      {{ useDateFormat(data!.date, 'MMMM DD, YYYY').value }}, by&nbsp;
-      <span
-        class="flex items-center cursor-pointer"
-        onclick="author_modal.showModal()"
-      >
+    <article class="max-w-screen-sm mx-auto px-6 pt-4 md:pt-6">
+      <div class="pb-8">
         <NuxtImg
-          v-if="authorPicture"
-          class="mx-2 w-9 h-9 rounded-full object-cover"
-          :src="authorPicture"
-          width="50px"
-          alt="Author portrait"
+          :src="imgPath"
+          alt="Header image"
+          width="600px"
         />
-        {{ author.name }}
-      </span>
-    </div>
-    <section class="text-gray-900 dark:text-gray-200 prose dark:prose-invert">
-      <ContentRenderer :value="data" />
-    </section>
-    <dialog
-      id="author_modal"
-      class="modal"
-    >
-      <div class="modal-box relative">
-        <div class="flex items-center gap-10">
+      </div>
+      <h1 class="mb-6 text-3xl font-semibold tracking-tight">
+        {{ data!.title }}
+      </h1>
+      <p class="mb-5 text-gray-800 dark:text-gray-300">
+        {{ data!.description }}
+      </p>
+      <div class="pb-2 flex flex-row items-center text-gray-500">
+        {{ useDateFormat(data!.date, 'MMMM DD, YYYY').value }}, by&nbsp;
+        <span
+          class="flex items-center cursor-pointer"
+          onclick="author_modal.showModal()"
+        >
           <NuxtImg
             v-if="authorPicture"
-            class="rounded-full object-cover"
+            class="mx-2 w-9 h-9 rounded-full object-cover"
             :src="authorPicture"
-            width="150px"
+            width="50px"
             alt="Author portrait"
           />
-          <div class="min-h-28">
-            <h3 class="text-lg font-bold">
-              {{ author.name }}
-            </h3>
-            <p class="pt-1 pb-3 capitalize text-gray-500 dark:text-gray-400 tracking-wide">
-              {{ author.role }}
-            </p>
-            <p class="text-sm text-gray-600 dark:text-gray-300">
-              {{ author.description }}
-            </p>
-            <TeamSocial
-              v-if="author.social.x || author.social.github || author.social.forum"
-              class="pt-4"
-              :member="author"
-            />
-          </div>
-        </div>
-        <form method="dialog">
-          <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-            ✕
-          </button>
-        </form>
+          {{ author.name }}
+        </span>
       </div>
-      <form
-        method="dialog"
-        class="modal-backdrop"
+      <section class="text-gray-900 dark:text-gray-200 prose dark:prose-invert">
+        <ContentRenderer :value="data" />
+      </section>
+      <dialog
+        id="author_modal"
+        class="modal"
       >
-        <button>close</button>
-      </form>
-    </dialog>
-  </article>
+        <div class="modal-box relative">
+          <div class="flex items-center gap-10">
+            <NuxtImg
+              v-if="authorPicture"
+              class="rounded-full object-cover"
+              :src="authorPicture"
+              width="150px"
+              alt="Author portrait"
+            />
+            <div class="min-h-28">
+              <h3 class="text-lg font-bold">
+                {{ author.name }}
+              </h3>
+              <p class="pt-1 pb-3 capitalize text-gray-500 dark:text-gray-400 tracking-wide">
+                {{ author.role }}
+              </p>
+              <p class="text-sm text-gray-600 dark:text-gray-300">
+                {{ author.description }}
+              </p>
+              <TeamSocial
+                v-if="author.social.x || author.social.github || author.social.forum"
+                class="pt-4"
+                :member="author"
+              />
+            </div>
+          </div>
+          <form method="dialog">
+            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+        </div>
+        <form
+          method="dialog"
+          class="modal-backdrop"
+        >
+          <button>close</button>
+        </form>
+      </dialog>
+    </article>
+  </div>
 </template>
 
 <script setup lang="ts">
