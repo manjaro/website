ac516283aced4de1266c80f6ed1ad11b905ef9c9
Roman Gilg <romangg@manjaro.org>
2024-06-28 11:48:02 +0200

    fix: align social icons

--- components/team-social.vue
+++ components/team-social.vue
@@ -1,9 +1,9 @@
 <template>
-  <span class="inline-flex">
+  <span class="inline-flex items-center gap-0.5">
     <NuxtLink
       v-if="member.social.x"
       :href="member.social.x"
-      class="text-gray-500"
+      class="text-gray-500 h-[18px] w-[18px]"
     >
       <svg
         fill="none"
@@ -11,8 +11,7 @@
         stroke-linecap="round"
         stroke-linejoin="round"
         stroke-width="1"
-        class="w-6 h-6"
-        viewBox="0 0 23 23"
+        viewBox="0 0 16 16"
       >
         <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
       </svg>
@@ -20,7 +19,7 @@
     <NuxtLink
       v-if="member.social.github"
       :href="member.social.github"
-      class="ml-2 text-gray-500"
+      class="ml-2 text-gray-500 h-5 w-5"
     >
       <svg
         fill="currentColor"
@@ -28,8 +27,7 @@
         stroke-linecap="round"
         stroke-linejoin="round"
         stroke-width="0.1"
-        class="w-6 h-6"
-        viewBox="0 0 20 20"
+        viewBox="0 0 16 16"
       >
         <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
       </svg>
@@ -37,18 +35,17 @@
     <NuxtLink
       v-if="member.social.forum"
       :href="member.social.forum"
-      class="ml-2 text-gray-500"
+      class="ml-2 text-gray-500 h-[22px] w-[22px]"
     >
       <svg
-        fill="none"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        class="w-5 h-5"
-        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        width="100%"
+        height="100%"
+        fill="currentColor"
+        viewBox="0 0 16 16"
       >
-        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+        <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105" />
+        <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8m0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5" />
       </svg>
     </NuxtLink>
   </span>
