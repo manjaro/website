c811876b2f3f36a458baa9fd1a584179a5f9f471
Roman Gilg <romangg@manjaro.org>
2024-06-30 11:57:02 +0200

    feat: restructure download paths
    Same scheme for x86 and ARM.

--- pages/products/download/x86.vue
+++ pages/products/download/x86.vue
@@ -20,14 +20,19 @@
             Products
           </NuxtLink>
         </li>
-        <li>Download</li>
+        <li>
+          <NuxtLink href="/products">
+            Download
+          </NuxtLink>
+        </li>
+        <li>x86</li>
       </ul>
     </div>
     <section class="text-gray-600 body-font dark:text-gray-400">
       <div class="container px-5 pt-8 mx-auto">
         <div class="flex flex-col text-center w-full mb-14">
           <h1 class="text-2xl font-medium title-font mb-4 text-gray-900 dark:text-gray-200 tracking-widest">
-            Official x86 Images
+            Official Images
           </h1>
           <p class="lg:w-2/3 mx-auto leading-relaxed">
             Provided by the Manjaro Team
@@ -73,7 +78,7 @@
       </div>
 
       <div class="container px-5 pt-20 mx-auto flex w-full mb-12 justify-center">
-        <NuxtLink href="/products/download-arm">
+        <NuxtLink href="/products/download/arm">
           <div class="btn btn-ghost leading-relaxed font-normal tracking-wider">
             Click here for ARM images
           </div>
