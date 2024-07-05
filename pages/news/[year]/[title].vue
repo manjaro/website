51f95d12fe5373400d4ecabd50c97f5326fc1863
Roman Gilg <romangg@manjaro.org>
2024-07-05 12:00:17 +0200

    fix: set data available
    Removes a warning about potential null type.

--- pages/news/[year]/[title].vue
+++ pages/news/[year]/[title].vue
@@ -35,7 +35,7 @@
           <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
         </svg>
         <li class="inline-flex items-center px-2 py-2 gap-1.5 text-center">
-          {{ data.title }}
+          {{ data!.title }}
         </li>
       </ol>
     </nav>
@@ -70,7 +70,7 @@
         </span>
       </div>
       <section class="text-gray-900 dark:text-gray-200 prose dark:prose-invert">
-        <ContentRenderer :value="data" />
+        <ContentRenderer :value="data!" />
       </section>
       <dialog
         id="author_modal"
